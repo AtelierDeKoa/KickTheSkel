f11ea78cadeaabb4f2b2eac27a2652efd23b4af2
Ana Martin <atelierdekoa@gmail.com>
2022-05-15 19:08:40 +0200

    Primer intento de animacion de esqueleto, counter, timer y pause

---
+++ script.js
@@ -0,0 +1,26 @@
+let counter = 0;
+let leftTime = 60;
+
+// Click en esqueleto
+$("#mov").click(function() {
+    counter++;
+    $("#counter").text(counter);
+    $(this).find("#skel01").css("display", "none");
+    $(this).find("#lilBomb01").css("display", "block");
+    $(this).css("animation-play-state", "paused").fadeOut(400);
+})
+
+// Timer
+const interval = setInterval(function() {
+    leftTime--;
+    $("#timer").text(leftTime);
+    if (leftTime == 0) {
+        clearInterval(interval);
+        $("#timer").text("Time out!");
+    }
+}, 1000);
+
+// Pause
+$("#pauseBtn").click(function() {
+    $("#pauseOpt").css("display", "flex");
+})
